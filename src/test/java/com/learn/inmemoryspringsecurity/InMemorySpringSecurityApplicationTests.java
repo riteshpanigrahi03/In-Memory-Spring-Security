b55b9d7242113477e622aec0fad64035d68bdad3
riteshpanigrahi03 <panigrahiritesh03@gmail.com>
2021-09-17 23:34:28 +0530

    initail commit

---
+++ src/test/java/com/learn/inmemoryspringsecurity/InMemorySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.inmemoryspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMemorySpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
