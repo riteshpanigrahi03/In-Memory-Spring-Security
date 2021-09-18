1e531ac9a81d50dae08e4e0389e8e3ed32b5489b
riteshpanigrahi03 <panigrahiritesh03@gmail.com>
2021-09-18 14:29:29 +0530

    add new user and configured authorization

--- src/main/java/com/learn/inmemoryspringsecurity/config/SpringSecurityConfig.java
+++ src/main/java/com/learn/inmemoryspringsecurity/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.learn.inmemoryspringsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +12,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("demo1").password("{noop}test1").roles("USER");
+            .withUser("demo1").password("{noop}test1").roles("USER")
+            .and()
+            .withUser("demo2").password("{noop}test2").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
